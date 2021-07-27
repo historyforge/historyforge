01d1ebf2f27ba765c2f6b0c8b5f6c1d1ece3de98
David Furber <david@tessellatestudio.com>
2021-07-27 14:42:24 -0400

    more refactor of auto strip attributes

--- app/models/concerns/auto_strip_attributes.rb
+++ app/models/concerns/auto_strip_attributes.rb
@@ -24,54 +24,34 @@ module AutoStripAttributes
 
   class Config
     class << self
-      attr_accessor :filters, :filters_enabled, :filters_order
+      attr_accessor :filters, :filters_order
 
       def setup
-        @filters ||= {}
-        @filters_enabled ||= {}
-        @filters_order ||= []
-
-        set_filter remove_periods: true do |value|
+        set_filter :remove_periods do |value|
           value.respond_to?(:gsub) ? value.gsub(/\./, '') : value
         end
-        set_filter convert_non_breaking_spaces: false do |value|
-          value.respond_to?(:gsub) ? value.gsub("\u00A0", ' ') : value
-        end
-        set_filter strip: true do |value|
+        set_filter :strip do |value|
           value.respond_to?(:strip) ? value.strip : value
         end
-        set_filter nullify: true do |value|
+        set_filter :nullify do |value|
           # We check for blank? and empty? because rails uses empty? inside blank?
           # e.g. MiniTest::Mock.new() only responds to .blank? but not empty?, check tests for more info
           # Basically same as value.blank? ? nil : value
           value.respond_to?(:'blank?') && value.respond_to?(:'empty?') && value.blank? ? nil : value
         end
-        set_filter squish: false do |value|
-          value.respond_to?(:gsub) ? value.gsub(/\s+/, ' ') : value
-        end
-        set_filter delete_whitespaces: false do |value|
-          value.respond_to?(:delete) ? value.delete(" \t") : value
-        end
       end
 
       def set_filter(filter, &block)
-        if filter.is_a?(Hash)
-          filter_name = filter.keys.first
-          filter_enabled = filter.values.first
-        else
-          filter_name = filter
-          filter_enabled = false
-        end
-        @filters[filter_name] = block
-        @filters_enabled[filter_name] = filter_enabled
+        @filters ||= {}
+        @filters_order ||= []
+
+        @filters[filter] = block
         # in case filter is redefined, we probably don't want to change the order
-        @filters_order << filter_name unless @filters_order.include? filter_name
+        @filters_order << filter unless @filters_order.include? filter
       end
 
-      def each_filter(&block)
+      def each_filter
         filters_order.each do |filter_name|
-          next unless filters_enabled[filter_name]
-
           yield filters[filter_name]
         end
       end
