d34a57df2f28401d2be7db47fb9c3b1a7754ca0b
David Furber <david@tessellatestudio.com>
2021-07-28 23:58:02 -0400

    photograph map issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
 
   def browser_title
@@ -25,19 +27,11 @@ module ApplicationHelper
     js = []
     js << ['success', message_for_item(flash[:notice], :notice_item)] unless flash[:notice].blank?
     js << ['error', message_for_item(flash[:errors], :errors_itme)] unless flash[:errors].blank?
-    js << ['log', message_for_item(flash[:alert], :alert_item)] unless flash[:alert].blank?
-    js << ['log', message_for_item(flash[:warning], :warning_item)] unless flash[:warning].blank?
     js
   end
 
   def flash_messages
     javascript_tag "window.alerts=#{prepare_alerts_base.to_json};"
-  end
-
-  def prepare_xhr_alerts
-    prepare_alerts_base.map { |key, value|
-      content_tag(:div, value, :class => 'alert hidden', 'data-alert' => key)
-    }.join('')
   end
 
   def admin_authorized?
@@ -47,6 +41,7 @@ module ApplicationHelper
   # Renders form using readonly, display-friendly view.
   def view_for(record, options = {}, &block)
     raise ArgumentError, 'Missing block' unless block_given?
+
     html_options = options[:html] ||= {}
     html_options[:class] ||= ''
     html_options[:class] << ' readonly-form'
@@ -68,37 +63,26 @@ module ApplicationHelper
 
   def message_for_item(message, item = nil)
     if item.is_a?(Array)
-     raw message % link_to(*item)
+      raw message % link_to(*item)
     else
       message % item
     end
-  end
-
-  def strip_brackets(str)
-    str ||=''
-    str.gsub(/[\]\[()]/,'')
   end
 
   def yes_or_no(value)
     value ? 'Yes' : '' # 'No'
   end
 
-  def snippet(thought, wordcount)
-    if thought
-      thought.split[0..(wordcount-1)].join(' ') +(thought.split.size > wordcount ? '...' : '')
-    end
-  end
-
   def error_messages_for(*objects)
     options = objects.extract_options!
-    options[:header_message] ||= I18n.t(:"activerecord.errors.header", :default => 'Invalid Fields')
-    options[:message] ||= I18n.t(:"activerecord.errors.message", :default => 'Correct the following errors and try again.')
+    options[:header_message] ||= I18n.t(:"activerecord.errors.header", default: 'Invalid Fields')
+    options[:message] ||= I18n.t(:"activerecord.errors.message", default: 'Correct the following errors and try again.')
     messages = objects.compact.map { |o| o.errors.full_messages }.flatten
-    unless messages.empty?
-      content_tag(:div, :class => 'error_messages') do
-        list_items = messages.map { |msg| content_tag(:li, msg) }
-        content_tag(:h2, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
-      end
+    return if messages.empty?
+
+    content_tag(:div, class: 'error_messages') do
+      list_items = messages.map { |msg| content_tag(:li, msg) }
+      content_tag(:h2, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
     end
   end
 
@@ -190,7 +174,7 @@ module ApplicationHelper
 
   def table(attrs={}, &block)
     attrs['data-sortable'] = true if attrs.delete(:sortable)
-    content_tag :table, capture(&block), attrs.merge(:class => 'table table-striped table-condensed')
+    content_tag :table, capture(&block), attrs.merge(class: 'table table-striped table-condensed')
   end
 
   def link_to_building(building)
--- app/helpers/photographs_helper.rb
+++ app/helpers/photographs_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module PhotographsHelper
   def thumb_for(photo)
     return unless photo.file.attached?
@@ -7,16 +9,9 @@ module PhotographsHelper
   end
 
   def review_error_messages_for(*objects)
-    options = objects.extract_options!
-    options[:header_message] ||= I18n.t(:"activerecord.errors.header", :default => "Review Status")
-    options[:message] ||= I18n.t(:"activerecord.errors.message", :default => "Things to do before we can mark this as reviewed:")
-    messages = objects.compact.map { |o| o.errors.full_messages }.flatten
-    unless messages.empty?
-      content_tag(:div, :class => "alert alert-info") do
-        list_items = messages.map { |msg| content_tag(:li, msg) }
-        content_tag(:h4, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
-      end
-    end
+    error_messages_for *objects,
+                       header_message: 'Review Status',
+                       message: 'Things to do before we can mark this as reviewed:'
   end
 
 end
