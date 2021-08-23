dc92b3ac4c796b889b249b651ed01d2dc63bb95b
David Furber <david@tessellatestudio.com>
2021-08-23 10:20:38 -0400

    decorator comments and refinements

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# Base class for decorators. Implements method missing to return nil. Kinda dangerous but different census years
+# have different fields. Sometimes people execute a query by changing the year in the URL. This causes it to try
+# to output fields that don't exist. Instead of barfing let's just ignore the field.
 class ApplicationDecorator < Draper::Decorator
   delegate_all
+
+  def method_missing(method, *args)
+    super
+  rescue NoMethodError
+    nil
+  end
+
+  def respond_to_missing?(method, include_private = nil)
+    super
+  end
 end
--- app/decorators/census_record_decorator.rb
+++ app/decorators/census_record_decorator.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+# All census years share this decorator even though they have different subclasses. The reasoning is simple: the same
+# field on each census year should have the same name and output using the same mechanism.
 class CensusRecordDecorator < ApplicationDecorator
   def name
-    "#{last_name}, #{first_name} #{middle_name}".strip
+    "#{[object.last_name, object.name_suffix].compact.join(' ')}, #{first_name} #{[middle_name, name_prefix].compact.join(', ')}".strip
   end
 
   def age
@@ -21,35 +23,37 @@ class CensusRecordDecorator < ApplicationDecorator
     object.locality&.name
   end
 
+  # Yes/no fields should output Yes if true otherwise nothing.
   %w[foreign_born can_read can_write can_speak_english foreign_born unemployed attended_school
      blind deaf_dumb has_radio lives_on_farm can_read_write
      worked_yesterday veteran residence_1935_farm private_work public_work
      seeking_work had_job had_unearned_income veteran_dead soc_sec deductions
      multi_marriage].each do |method|
     define_method method do
-      yes_or_blank object.send(method)
+      object.send(method) && 'Yes' || nil
     end
   end
 
+  # Coded answers (enumerations) should output the full text rather than the code.
   %w[race marital_status sex naturalized_alien employment worker_class
      owned_or_rented mortgage farm_or_house civil_war_vet war_fought].each do |method|
     define_method method do
-      census_code object.public_send(method), method
+      translate_census_code object.public_send(method), method
     end
   end
 
+  # Outputs a phrase combining ward, enumeration district, sheet, side, and line number so we don't have to occupy
+  # multiple columns with this information.
   def census_scope
     str = []
     str << "Ward #{object.ward} " if object.ward.present?
-    str << "ED #{object.enum_dist} p. #{object.page_number}#{object.page_side} # #{object.line_number}"
+    str << "ED #{object.enum_dist} sh. #{object.page_number}#{object.page_side} # #{object.line_number}"
     str.join
   end
 
-  def yes_or_blank(value)
-    value && 'Yes' || nil
-  end
+  private
 
-  def census_code(value, method)
+  def translate_census_code(value, method)
     return '' if value.blank?
 
     I18n.t("#{method}.#{value.downcase.gsub(/\W/, '')}", scope: 'census_codes', default: value)
