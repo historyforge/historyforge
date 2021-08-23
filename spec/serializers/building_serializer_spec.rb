2f6e67e7cac002aee4308561d522a19596229073
David Furber <david@tessellatestudio.com>
2021-08-22 20:43:49 -0400

    fix a spec

--- spec/serializers/building_serializer_spec.rb
+++ spec/serializers/building_serializer_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe BuildingSerializer do
   subject { BuildingSerializer.new(building).as_json }
 
   it 'has the correct fields' do
-    expect(subject['id']).to eq(building.id.to_s)
-    expect(subject['frame']).to eq(building.frame_type.name)
-    expect(subject['lining']).to eq(building.lining_type.name)
+    expect(subject['id']).to eq(building.id)
+    expect(subject['frame']).to eq(building.frame)
+    expect(subject['lining']).to eq(building.lining)
     expect(subject['building_type_ids']).to eq(building.building_type_ids)
-    expect(subject['type']).to eq(building.building_types.map(&:name).join(', '))
+    expect(subject['type']).to eq(building.type)
     expect(subject['stories']).to eq(building.stories)
     expect(subject['year_earliest']).to eq(building.year_earliest)
     expect(subject['year_latest']).to eq(building.year_latest)
     expect(subject['street_address']).to eq(building.street_address)
 
-    expect(subject['architects']).to eq(building.architects.first.name)
+    expect(subject['architects']).to eq(building.architects)
   end
 end
