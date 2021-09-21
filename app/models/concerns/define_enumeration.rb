a13a885b146d5390c2ac597fa08d3ccb40e32244
David Furber <david@tessellatestudio.com>
2021-09-21 11:19:29 -0400

    page_side issue uncovered an issue with define_enumeration"

--- app/models/concerns/define_enumeration.rb
+++ app/models/concerns/define_enumeration.rb
@@ -2,12 +2,24 @@ module DefineEnumeration
   extend ActiveSupport::Concern
   included do
     class_attribute :enumerations
-    def self.define_enumeration(name, values, strict=false)
+    def self.define_enumeration(name, values, strict = false)
       self.enumerations ||= []
-      self.enumerations << name unless enumerations.include?(name)
-      class_eval "def self.#{name}_choices; #{values.inspect}; end"
-      if strict
-        validates_inclusion_of name, in: values
+
+      class_eval "def self.#{name}_choices; #{values.inspect}; end", __FILE__, __LINE__
+
+      if enumerations.include?(name)
+        replace_inclusion_validator(name, values) if strict
+      else
+        self.enumerations << name
+        class_eval "validates_inclusion_of :#{name}, in: #{name}_choices", __FILE__, __LINE__ if strict
+      end
+    end
+
+    def self.replace_inclusion_validator(attr, values)
+      validator = _validators[attr]&.detect { |v| v.is_a?(ActiveModel::Validations::InclusionValidator) }
+      if validator
+        validator.instance_variable_set(:"@options", { in: values })
+        validator.instance_variable_set(:"@delimiter", values)
       end
     end
   end
