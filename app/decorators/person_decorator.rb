7446d9a46d863213cede6ecb42c07461619075af
David Furber <david@tessellatestudio.com>
2022-10-30 16:28:31 -0400

    name formatting on person

--- app/decorators/person_decorator.rb
+++ app/decorators/person_decorator.rb
@@ -2,7 +2,15 @@
 
 class PersonDecorator < ApplicationDecorator
   def name
-    "#{last_name}, #{first_name} #{middle_name}".strip
+    [last_name_parts, rest_of_name_parts].compact_blank.join(', ').strip
+  end
+
+  def last_name_parts
+    [last_name, name_suffix].compact_blank.join(' ')
+  end
+
+  def rest_of_name_parts
+    [first_name, [middle_name, name_prefix].compact_blank.join(', ')].compact_blank.join(' ')
   end
 
   def reviewed?
