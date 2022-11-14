d9e7afb6775466f70feff494100292f7fcfeaa54
David Furber <david@tessellatestudio.com>
2022-11-13 21:54:33 -0500

    merge people fix

--- app/services/merge_eligibility_check.rb
+++ app/services/merge_eligibility_check.rb
@@ -7,10 +7,10 @@ class MergeEligibilityCheck
 
   def perform
     @clashes = []
-    @clashes << 1900 if @source.census1900_record && @target.census1900_record
-    @clashes << 1910 if @source.census1910_record && @target.census1910_record
-    @clashes << 1920 if @source.census1920_record && @target.census1920_record
-    @clashes << 1930 if @source.census1930_record && @target.census1930_record
+    @clashes << 1900 if @source.census1900_records && @target.census1900_records
+    @clashes << 1910 if @source.census1910_records && @target.census1910_records
+    @clashes << 1920 if @source.census1920_records && @target.census1920_records
+    @clashes << 1930 if @source.census1930_records && @target.census1930_records
   end
 
   def okay?
