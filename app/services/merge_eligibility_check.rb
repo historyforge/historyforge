b1bfe4a2ac995275cb0aab18def9b0d791434a87
David Furber <david@tessellatestudio.com>
2022-11-13 22:03:28 -0500

    test merge eligibility check

--- app/services/merge_eligibility_check.rb
+++ app/services/merge_eligibility_check.rb
@@ -7,10 +7,10 @@ class MergeEligibilityCheck
 
   def perform
     @clashes = []
-    @clashes << 1900 if @source.census1900_records && @target.census1900_records
-    @clashes << 1910 if @source.census1910_records && @target.census1910_records
-    @clashes << 1920 if @source.census1920_records && @target.census1920_records
-    @clashes << 1930 if @source.census1930_records && @target.census1930_records
+    @clashes << 1900 if @source.census1900_records.exists? && @target.census1900_records.exists?
+    @clashes << 1910 if @source.census1910_records.exists? && @target.census1910_records.exists?
+    @clashes << 1920 if @source.census1920_records.exists? && @target.census1920_records.exists?
+    @clashes << 1930 if @source.census1930_records.exists? && @target.census1930_records.exists?
   end
 
   def okay?
---
+++ spec/services/merge_eligibility_check_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MergeEligibilityCheck do
+  let(:source) { create(:person, description: 'source text') }
+  let(:target) { create(:person, description: 'target text') }
+  let!(:source_record) { create(:census1910_record, person: source) }
+  let!(:target_record) { nil }
+  subject { described_class.new(source, target) }
+  before do
+    PaperTrail.request.whodunnit = create(:user)
+  end
+  describe '#okay?' do
+    context 'not okay because of census records in the same year' do
+      let!(:target_record) { create(:census1910_record, person: target) }
+      before { subject.perform }
+      it 'says no' do
+        expect(subject.okay?).to be_falsey
+      end
+    end
+    context 'is okay because no census records in the same year' do
+      let!(:target_record) { create(:census1920_record, person: target) }
+      before { subject.perform }
+      it 'says no' do
+        expect(subject.okay?).to be_truthy
+      end
+    end
+  end
+end
