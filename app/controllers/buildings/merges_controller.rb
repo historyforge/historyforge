67542e54ee6629ae7f411fec4e4d8375c43f6271
David Furber <david@tessellatestudio.com>
2023-01-15 14:57:23 -0500

    convert building merge to interaction

--- app/controllers/buildings/merges_controller.rb
+++ app/controllers/buildings/merges_controller.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
 module Buildings
+  # Handles the merging of buildings.
   class MergesController < ApplicationController
     before_action :load_and_authorize
 
-    def new
-    end
+    def new; end
 
     def create
-      if @check.okay?
-        MergeBuilding.new(@source, @target).perform
+      if Buildings::Merge.run!(source: @source, target: @target)
         flash[:notice] = 'The merge operation has been performed.'
         redirect_to @target
       else
-        flash[:errors] = "You can't merge these buildings."
+        flash[:errors] = "You can't merge these buildings. They aren't close enough to be the same building."
         redirect_back fallback_location: { action: :new }
       end
     end
@@ -24,8 +23,6 @@ module Buildings
       authorize! :merge, Building
       @target = Building.find(params[:building_id])
       @source = Building.find(params[:merge_id])
-      @check = BuildingMergeEligibilityCheck.new(@source, @target)
-      @check.perform
     end
   end
 end
---
+++ app/services/buildings/merge.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module Buildings
+  class Merge < ApplicationInteraction
+    object :source, class: 'Building'
+    object :target, class: 'Building'
+
+    def execute
+      merge_census_records
+      merge_addresses
+      merge_building_data
+      merge_photographs
+      target.save!
+      source.destroy!
+      BuildAddressHistory.new(target).perform
+      target.audit_logs.create message: "Merged ##{source.id} - #{source.name}"
+    end
+
+    private
+
+    def validate_distance
+      errors.add(:base, 'Buildings are too far apart to merge.') if distance > 0.015
+    end
+
+    def distance
+      Geocoder::Calculations.distance_between(source, target)
+    end
+
+    def merge_addresses
+      source.addresses.each do |address|
+        next if target.addresses.any? { |addr| addr.equals?(address) }
+
+        address.is_primary = false
+        target.addresses << address
+      end
+    end
+
+    def merge_building_data
+      source.attributes.keys.each do |attr|
+        next if attr =~ /created|updated|reviewed|address/
+
+        create_or_append(attr)
+      end
+      create_or_append(:architect_ids)
+    end
+
+    def merge_census_records
+      CensusYears.each do |year|
+        "Census#{year}Record".constantize.where(building_id: source.id).each do |record|
+          record.update(building_id: target.id)
+        end
+      end
+    end
+
+    def merge_photographs
+      @source.photos.each do |photo|
+        photo.buildings << @target
+      end
+    end
+
+    def create_or_append(field)
+      source_val = @source.send(field)
+      target_val = @target.send(field)
+      return if source_val.blank? || source_val == target_val
+
+      if target_val.blank?
+        @target.send("#{field}=", source_val)
+      elsif source_val.is_a?(String)
+        @target.send("#{field}=", [target_val, source_val].join("\r\n"))
+      elsif source_val.is_a?(Array)
+        @target.send("#{field}=", target_val.concat(source_val).uniq)
+      end
+    end
+  end
+end
