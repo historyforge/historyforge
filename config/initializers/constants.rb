e117154d3be4f1a18a4929e9d740b6758d886cba
David Furber <furberd@gmail.com>
2023-04-16 19:02:22 -0400

    add street suffixes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
 STREET_PREFIXES = %w[N S E W].freeze
-STREET_SUFFIXES = %w[Ave Blvd Ct Dr Hill Ln Pl Pk Rd Sq St Terr Tr Way].freeze
+STREET_SUFFIXES = %w[Aly Ave Blvd Cir Ct Dr Hill Ln Pl Pk Pkwy Rd Sq St Terr Tr Via Way].freeze
