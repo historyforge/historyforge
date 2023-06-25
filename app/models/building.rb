36428da47b4fa4082126557858a8c9ea7c5f2edf
David Furber <david@tessellatestudio.com>
2023-06-24 22:03:34 -0400

    but address without year goes last because it is the original

--- app/models/building.rb
+++ app/models/building.rb
@@ -248,7 +248,7 @@ class Building < ApplicationRecord
   end
 
   def street_address
-    addresses.sort { |a,b| b.year <=> a.year }.map(&:address_with_year).join("\n")
+    addresses.sort { |a,b| b.year.blank? ? -1 : b.year <=> a.year }.map(&:address_with_year).join("\n")
   end
 
   def primary_street_address
