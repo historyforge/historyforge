57be1721e8f115571d4cd64e6c3a9ff86e8979ca
David Furber <furberd@gmail.com>
2024-01-28 11:56:01 -0500

    failed migration

--- db/migrate/20240121141010_add_slugs_to_locations.rb
+++ db/migrate/20240121141010_add_slugs_to_locations.rb
@@ -1,9 +1,17 @@
 class AddSlugsToLocations < ActiveRecord::Migration[7.0]
   def change
-    add_column :localities, :slug, :string, index: { unique: true }
+    change_table :localities do |t|
+      t.string :slug, index: { unique: true }
+      t.string :short_name
+      t.boolean :primary, default: false, null: false
+    end
     reversible do |dir|
       dir.up do
-        Locality.find_each(&:save)
+        Locality.first.update(primary: true)
+        Locality.find_each do |row|
+          row.short_name = row.name
+          row.save
+        end
       end
     end
   end
