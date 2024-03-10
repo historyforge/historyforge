8570fdbd945f2084e2c54e1d57533dc9a489cb57
David Furber <furberd@gmail.com>
2024-03-10 17:03:03 -0400

    upgrade spam protection on contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -13,6 +13,9 @@
 class Contact < ApplicationRecord
   include ArDocStore::Model
 
+  SPAM_REGEX = /mailto|href|WCRTEST|http/
+  URI_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
+
   json_attribute :subject
   json_attribute :message
   json_attribute :name
@@ -22,4 +25,14 @@ class Contact < ApplicationRecord
   json_attribute :how_heard
 
   validates :subject, :message, :name, :email, presence: true
+  validate :validate_spam
+
+  def validate_spam
+    errors.add(:message, 'Mmmm delicious.') if smells_spammy?
+  end
+
+  def smells_spammy?
+    message =~ SPAM_REGEX || message =~ URI_REGEX
+  end
+
 end
