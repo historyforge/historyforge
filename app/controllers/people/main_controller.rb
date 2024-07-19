1c18bad42fbbf75f1788b8f479efa523ec525f7e
David Furber <furberd@gmail.com>
2024-07-18 21:59:57 -0400

    save person locality ids

--- app/controllers/people/main_controller.rb
+++ app/controllers/people/main_controller.rb
@@ -93,7 +93,8 @@ module People
       params.require(:person).permit :first_name, :last_name, :middle_name,
                                      :sex, :race, :name_prefix, :name_suffix, :birth_year, :is_birth_year_estimated,
                                      :pob, :is_pob_estimated, :notes, :description,
-                                     names_attributes: %i[_destroy id first_name last_name]
+                                     names_attributes: %i[_destroy id first_name last_name],
+                                     locality_ids: []
     end
   end
 end
