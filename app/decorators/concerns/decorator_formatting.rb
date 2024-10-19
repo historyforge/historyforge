208d24b307d98ca10ad82dceca860792126a0158
David Furber <furberd@gmail.com>
2024-10-19 15:29:01 -0400

    name again

--- app/decorators/concerns/decorator_formatting.rb
+++ app/decorators/concerns/decorator_formatting.rb
@@ -2,13 +2,9 @@
 
 module DecoratorFormatting
   def format_name(last_name:, middle_name: nil, first_name:, name_prefix: nil, name_suffix: nil)
-    name = last_name&.dup || ''
-    name << ' ' << name_suffix if name_suffix.present?
-    name << ', '
-    name << first_name if first_name.present?
-    name << ' ' << middle_name if middle_name.present?
-    name << ' ' << name_prefix if name_prefix.present?
-    name.strip.sub(/,$/, '')
+    name = [[last_name, name_suffix].compact_blank.join(' ')]
+    name << [first_name, middle_name, name_prefix].compact_blank.join(' ')
+    name.join(', ').strip.sub(/,$/, '')
   end
 
   def name
