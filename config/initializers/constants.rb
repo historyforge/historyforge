f0a7d3b91de8bd436c7320e89fe431d0e0fbf1c9
David Furber <furberd@gmail.com>
2024-11-23 18:31:57 -0500

    gmail support, add point street suffix

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,2 @@
 STREET_PREFIXES = %w[N S E W].freeze
-STREET_SUFFIXES = %w[Aly Ave Blvd Cir Ct Dr Hill Ln Pl Pk Pkwy Rd Sq St Terr Tr Via Walk Way].freeze
+STREET_SUFFIXES = %w[Aly Ave Blvd Cir Ct Dr Hill Ln Pl Pk Pkwy Pt Rd Sq St Terr Tr Via Walk Way].freeze
