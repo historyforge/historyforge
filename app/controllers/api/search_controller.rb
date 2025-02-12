96b808a6dcd4e80142b37916d9f53dc6c49b9279
WoodGreenDev <joshua.williams51GW@outlook.com>
2025-02-12 16:36:20 -0600

    refactoring_census_and_building_query_loops_into_method

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -2,24 +2,13 @@ module Api
   class SearchController < ApplicationController
    
     def search
-      @census_query = ""
-      @building_query = ""
-      Census1920Record.column_names.each do |name|
-        if  Census1920Record.columns_hash[name].type == :string || Census1920Record.columns_hash[name].type == :text
-            query= "census_1920_records.#{name} LIKE :search OR "
-            @census_query = @census_query.concat(query) 
-        end
-      end
-
+      @census_query = ''
+      @building_query = ''
+      @census_query = search_query('Census1920Record',@census_query)
+      @building_query = search_query('Building',@building_query)
      
-      Building.column_names.each do |name|
-        if  Building.columns_hash[name].type == :string || Building.columns_hash[name].type == :text
-            query= "buildings.#{name} LIKE :search OR "
-            @building_query = @building_query.concat(query) 
-        end
-      end
       @building_query = @building_query.chomp("OR ")
-     @census_query = @census_query.chomp("OR ")
+      @census_query = @census_query.chomp("OR ")
       if params["place"].present?
          @buildings = Building.where(@building_query,:search => "%#{params["place"]}%").ids.uniq
          @buildings2 = Building.joins(:census1920_records).where(@census_query,:search => "%#{params["place"]}%").ids.uniq
@@ -40,6 +29,19 @@ module Api
       render json: @geojson
     end
 
+
+    private def search_query(class_name,chosen_query)
+      class_object = class_name.constantize
+      table_name = class_object.table_name
+      class_object.column_names.each do |name|
+        if  class_object.columns_hash[name].type == :string || class_object.columns_hash[name].type == :text
+            query= "#{table_name}.#{name} ILIKE :search OR "
+            chosen_query = chosen_query.concat(query) 
+        end
+      end
+      chosen_query
+    
+    end
    
 
     private def build_geojson
