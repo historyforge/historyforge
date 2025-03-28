76d823b316e573864128e0ba2715d186603fb1e2
David Furber <furberd@gmail.com>
2025-03-27 22:42:29 -0400

    add pending migration check

--- spec/services/people/likely_matches_spec.rb
+++ spec/services/people/likely_matches_spec.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 module People
   RSpec.describe LikelyMatches do
     subject { outcome[:matches].first }
     let(:outcome) { described_class.run!(record:) }
-    let(:record) { FactoryBot.create(:census1910_record, first_name: 'David', last_name: 'Furber', sex: 'm', race: 'w') }
-    let(:non_matching_person) { FactoryBot.create(:person, first_name: 'Michael', last_name: 'Furber', sex: 'm', race: 'W') }
+    let(:record) { FactoryBot.create(:census1910_record, first_name: "David", last_name: "Furber", sex: "m", race: "W") }
+    let(:non_matching_person) { FactoryBot.create(:person, first_name: "Michael", last_name: "Furber", sex: "m", race: "W") }
 
     before do
+      Census1910Record.where(first_name: "David", last_name: "Furber", sex: "m", race: "W").delete_all
       # Automatic matching only engages when there is more than one census going.
       # Otherwise, partners get confused by "mystery matches" when logically
       # there shouldn't be an existing person record for anyone yet. That's why
@@ -19,26 +20,26 @@ module People
       non_matching_person
     end
 
-    context 'when there is an exact match' do
-      let(:matching_person) { FactoryBot.create(:person, first_name: 'David', last_name: 'Furber', sex: 'm', race: 'W') }
+    context "when there is an exact match" do
+      let(:matching_person) { FactoryBot.create(:person, first_name: "David", last_name: "Furber", sex: "m", race: "W") }
 
       it { is_expected.to eq(matching_person) }
     end
 
-    context 'when there is a same last name and cognate first name match' do
-      let!(:matching_person) { FactoryBot.create(:person, first_name: 'Dave', last_name: 'Furber', sex: 'm', race: 'W') }
+    context "when there is a same last name and cognate first name match" do
+      let!(:matching_person) { FactoryBot.create(:person, first_name: "Dave", last_name: "Furber", sex: "m", race: "W") }
 
       it { is_expected.to eq(matching_person) }
     end
 
-    context 'when there is a same last name and cognate middle name match' do
-      let!(:matching_person) { FactoryBot.create(:person, first_name: 'B', middle_name: 'Dave', last_name: 'Furber', sex: 'm', race: 'W') }
+    context "when there is a same last name and cognate middle name match" do
+      let!(:matching_person) { FactoryBot.create(:person, first_name: "B", middle_name: "Dave", last_name: "Furber", sex: "m", race: "W") }
 
       it { is_expected.to eq(matching_person) }
     end
 
-    context 'when there is a similar last name and cognate first name match' do
-      let!(:matching_person) { FactoryBot.create(:person, first_name: 'Dave', last_name: 'Ferber', sex: 'm', race: 'W') }
+    context "when there is a similar last name and cognate first name match" do
+      let!(:matching_person) { FactoryBot.create(:person, first_name: "Dave", last_name: "Ferber", sex: "m", race: "W") }
 
       it { is_expected.to eq(matching_person) }
     end
