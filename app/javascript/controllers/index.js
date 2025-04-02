42d18732472473978dc84fcac31864fd87bd5708
Keith Lewis <kclewis0614@gmail.com>
2025-04-01 21:51:35 -0500

    fix missing file errors

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,7 +5,6 @@ import BlowupController from "./blowup_controller";
 import CensusAutoCompleteController from "./census_autocomplete_controller";
 import LoadSavedSearchController from "./load_saved_search_controller";
 import PhotoWizardController from "./photo_wizard_controller";
-import DocumentController from "./document_controller.js";
 import SaveSearchController from "./save_search_controller";
 import SavedSearchesController from "./saved_searches_controller";
 
@@ -15,7 +14,6 @@ const definitions = [
     { identifier: 'census-autocomplete', controllerConstructor: CensusAutoCompleteController },
     { identifier: 'load-saved-search', controllerConstructor: LoadSavedSearchController },
     { identifier: 'photo-wizard', controllerConstructor: PhotoWizardController },
-    { identifier: 'document', controllerConstructor: DocumentController },
     { identifier: 'save-search', controllerConstructor: SaveSearchController },
     { identifier: 'saved-searches', controllerConstructor: SavedSearchesController },
 ]
