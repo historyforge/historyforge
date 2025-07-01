de438357333abc33bd327ddab0d9a31e0e8e3548
Keith Lewis <kclewis0614@gmail.com>
2025-07-01 17:06:31 -0500

    Cleanup unused artifacts

--- .ruby-lsp.rb
+++
@@ -1,9 +0,0 @@
-# .ruby-lsp.rb
-require "rubocop"
-puts "[ruby-lsp] Loaded .ruby-lsp.rb"
-
-# Force-load this specific constant to prevent autoload race
-RuboCop::Cop::Offense
-
-# Force-load this specific constant to prevent autoload race
-RuboCop::Cop::Error
--- scripts/ruby-lsp-test.rb
+++
@@ -1,3 +0,0 @@
-puts "Starting..."
-require "ruby-lsp"
-puts "Loaded Ruby LSP."
