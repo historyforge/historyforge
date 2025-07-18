a7db948063008863d8d0bddc43db405942d67a5b
David Furber <furberd@gmail.com>
2025-07-17 20:42:45 -0400

    recaptcha revisions

--- app/controllers/concerns/recaptcha_handlers.rb
+++ app/controllers/concerns/recaptcha_handlers.rb
@@ -5,17 +5,9 @@ module RecaptchaHandlers
 
   included do
     before_action :lazy_configure_recaptcha, if: :using_recaptcha?
-    before_action :check_captcha, only: :create, if: :using_recaptcha?
   end
 
   private
-
-  def check_captcha
-    return if verify_recaptcha(action: 'contact', minimum_score: 0.5, secret_key: AppConfig[:recaptcha_secret_key])
-
-    flash[:error] = 'Oops did you fill out the form correctly?'
-    render action: :new
-  end
 
   def using_recaptcha?
     AppConfig[:recaptcha_site_key].present? && AppConfig[:recaptcha_secret_key].present?
@@ -25,6 +17,7 @@ module RecaptchaHandlers
     Recaptcha.configure do |config|
       config.site_key = AppConfig[:recaptcha_site_key]
       config.secret_key = AppConfig[:recaptcha_secret_key]
+      config.skip_verify_env = %w[test]
     end
   end
 end
